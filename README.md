49bf45eb0058fa2c4206a7d6cc1fe383d84619cc
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2023-07-07 13:09:28 -0600

    migrate

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 <ol>
   <li>Checar que exista .env</li>
+  <li>Crear una base de datos llamada 'db_test'</li>
   <li>Tener instalado php 8</li>
   <li>Tener instalado Composer</li>
   <li>Correr 'composer install'</li>
+  <li>Correr 'php artisan migrate'</li>
   <li>Correr 'php artisan serve'</li>
 </ol>
---
+++ database/migrations/2023_07_07_190720_create_empleados_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('clave_del_empleado');
+            $table->string('nombre');
+            $table->string('edad');
+            $table->string('fecha_nacimiento');
+            $table->string('genero');
+            $table->string('sueldo_base');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
